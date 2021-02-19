9a8b133dd6a7ce3ef5496d531ef28c2184ffce45
HAKJOON0223 <2hakjoon@gmail.com>
2021-02-19 23:44:09 +0900

    add gulp image

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,18 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import gimage from "gulp-image";
+
 
 const routes = {
     pug : {
+        watch : "src/**/*.pug",
         src:"src/*.pug",
         dest : "build"
+    },
+    img:{
+        src: "src/img/*",
+        dest: "build/img"
     }
 }
 
@@ -14,12 +21,18 @@ const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pu
 
 const clean = () => del(["build"]);
 
-const webserver = () => gulp.src("build").pipe(ws({livereload:true, open:true}))
+const webserver = () => gulp.src("build").pipe(ws({livereload:true, open:true}));
 
-const prepare = gulp.series([clean]);
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+};
+
+const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver]);
+const postDev = gulp.series([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
