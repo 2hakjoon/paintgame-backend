1e8c79bbb70e8e8d7166710946574bd8caf3ef61
HAKJOON0223 <2hakjoon@gmail.com>
2021-02-20 01:14:50 +0900

    add gulp scss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,12 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import gimage from "gulp-image";
+import sass from "gulp-sass";
+import autop from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
 
+
+sass.compiler = require("node-sass");
 
 const routes = {
     pug : {
@@ -14,6 +19,11 @@ const routes = {
     img:{
         src: "src/img/*",
         dest: "build/img"
+    },
+    scss:{
+        watch:"src/scss/**/*.scss",
+        src: "src/scss/styles.scss",
+        dest: "build/css",
     }
 }
 
@@ -21,19 +31,34 @@ const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pu
 
 const clean = () => del(["build"]);
 
-const webserver = () => gulp.src("build").pipe(ws({livereload:true, open:true}));
-
-const img = () => gulp.src(routes.img.src).pipe(gimage()).pipe(gulp.dest(routes.img.dest));
-
-const watch = () => {
-    gulp.watch(routes.pug.watch, pug);
-    gulp.watch(routes.img.src, img);
+const webserver = () => {
+    gulp.src("build")
+    .pipe(ws({livereload:true, open:true}))
 };
 
+const img = () => 
+    gulp.src(routes.img.src)
+    .pipe(gimage())
+    .pipe(gulp.dest(routes.img.dest));
 
-const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const styles = () => 
+    gulp.src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autop())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+
+const watch = () => 
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, img);
+    gulp.watch(routes.scss.watch, styles);
+
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug, styles, img]);
 
 const postDev = gulp.series([webserver, watch]);
 
