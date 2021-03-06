9f5995c5828d21e172b659d56dc114ee5e26561d
HAKJOON0223 <2hakjoon@gmail.com>
2021-03-07 02:36:52 +0900

    add countDown

--- src/socket/gameNotice.js
+++ src/socket/gameNotice.js
@@ -2,6 +2,6 @@ const announcer = "-진행자-";
 const color = "red"
 
 export const notice = {
-        gameStart : {user : announcer, color : color ,text:"게임이 곧 시작됩니다."},
-
+    freeNotice : {user : announcer, color : color},
+    gameStart : {user : announcer, color : color ,text:"게임이 곧 시작됩니다."},
 }
--- src/socket/socketEvents.js
+++ src/socket/socketEvents.js
@@ -3,13 +3,34 @@ import { rndColor } from "../Colors";
 import commends from "./commend";
 import { notice } from "./gameNotice";
 
+
+let gameState = false;
 let userList = [
     {
     userId: 'sag',
     userColor: 'violet',
     socket: 'XPMWtaGuYfHyTy15AAAj'
-}
+} 
 ];
+
+
+
+const gameStart = (io) => {
+    io.emit(commends.gameStarted, {data : notice.gameStart})
+    setTimeout(()=>countDown(io, 3), 1000);
+}
+
+const countDown = (io, sec) => {
+    let cnt = sec;
+    const count = setInterval(()=>{
+        io.emit(commends.countDown, {data : {...notice.freeNotice, text: cnt}})
+        cnt = cnt-1;
+        if(cnt == 0){
+            clearInterval(count);
+        }
+    }, 1000);
+}
+
 export const socketController = (socket, io) => {
     socket.on(commends.setNickname, (data) => {
         const color = rndColor();
@@ -22,8 +43,9 @@ export const socketController = (socket, io) => {
         console.log(userList)
         socket.emit(commends.nicknameConfirm, color);
         socket.broadcast.emit(commends.playerUpdate, {userId : data, color})
-        if(userList.length >= 2){
-            io.emit(commends.gameStarted, {data : notice.gameStart})
+        
+        if(userList.length >= 2 && gameState==false){
+            gameStart(io);
         }
     });
 
