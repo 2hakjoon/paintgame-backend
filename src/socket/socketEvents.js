e499c3c0da3832048dda4abb3ca76cf77d3c3f16
HAKJOON0223 <2hakjoon@gmail.com>
2021-03-09 23:25:02 +0900

    making score borad

--- src/socket/socketEvents.js
+++ src/socket/socketEvents.js
@@ -11,20 +11,24 @@ let word = "";
 
 
 const gameStart = (io) => {
-    gameState = true;
-    io.emit(commends.gameStarted, {data : notice.gameStart})
-    setTimeout(()=>countDown(io, 3), 1000);
-    setTimeout(()=>selcetPainter(io), 5000);
-    setTimeout(()=>countDown(io, 60), 4000);
+    if(userList.length >= 2 && gameState==false){
+        gameState = true;
+        io.emit(commends.gameStarted, {data : notice.gameStart})
+        setTimeout(()=>countDown(io, 3), 1000);
+        setTimeout(()=>selcetPainter(io), 5000);
+        setTimeout(()=>countDown(io, 60), 4000);
+    }
 }
 
 const gameEnd = (io, user) => {
     gameState = false;
     io.emit(commends.countDown, ``)
     io.emit(commends.painterNotif, ``)
-    io.emit(commends.newMsg, {data : {...notice.answered, text: `${user}님이 정답을 맞추셨습니다!` }})
-
+    if(user!==undefined){
+        io.emit(commends.newMsg, {data : {...notice.answered, text: `${user}님이 정답을 맞추셨습니다!` }})
+    }
     scoreBoard(io);
+    setTimeout(()=>gameStart(io), 2000);
 }
 
 const countDown = (io, sec) => {
@@ -34,10 +38,16 @@ const countDown = (io, sec) => {
             io.emit(commends.countDown, `남은시간 : ${cnt} 초`)
         }
         else{
-            io.emit(commends.newMsg, {data : {...notice.freeNotice, text: cnt}})
+            if(gameState == true){
+                io.emit(commends.newMsg, {data : {...notice.freeNotice, text: cnt}})
+            }
         }
 
         cnt = cnt-1;
+        if((cnt == 0) && (sec == 60)){
+            io.emit(commends.newMsg, {data : {...notice.freeNotice, text: `정답은 < ${word} > 였습니다.!`}})
+            gameEnd(io);
+        }
         if((cnt == 0) || (gameState == false)){
             io.emit(commends.countDown, ``)
             clearInterval(count);
@@ -47,19 +57,20 @@ const countDown = (io, sec) => {
 }
 
 const selcetPainter = (io) => {
-    word = rndWord();
-    const painter = userList[Math.floor(Math.random() * userList.length)];
-    userList.map((user)=>{
-        if(user.socket === painter.socket){
-            io.to(painter.socket).emit(commends.newMsg, {data : {...notice.freeNotice, text: "당신이 그릴 차례입니다."}})
-            io.to(painter.socket).emit(commends.painterNotif, `제시어 : ${word}`)
-        }
-        else{
-            io.to(user.socket).emit(commends.newMsg, {data : {...notice.freeNotice, text: "정답을 맞춰보세요!"}, word:(rndWord().length+1)})
-            io.to(user.socket).emit(commends.painterNotif, `제시어 : ${word.length}글자`)
-        }
-    })
-    
+    if(gameState == true){
+        word = rndWord();
+        const painter = userList[Math.floor(Math.random() * userList.length)];
+        userList.map((user)=>{
+            if(user.socket === painter.socket){
+                io.to(painter.socket).emit(commends.newMsg, {data : {...notice.freeNotice, text: "당신이 그릴 차례입니다."}})
+                io.to(painter.socket).emit(commends.painterNotif, `제시어 : ${word}`)
+            }
+            else{
+                io.to(user.socket).emit(commends.newMsg, {data : {...notice.freeNotice, text: "정답을 맞춰보세요!"}, word:(rndWord().length+1)})
+                io.to(user.socket).emit(commends.painterNotif, `제시어 : ${word.length}글자`)
+            }
+        })
+    }
 }
 
 const scoreBoard = (io) => {
@@ -81,9 +92,9 @@ export const socketController = (socket, io) => {
         socket.emit(commends.nicknameConfirm, color);
         socket.broadcast.emit(commends.playerUpdate, {data : {...notice.freeNotice, text: `${userId}님이 입장하셨습니다.`}})
         scoreBoard(io);
-        if(userList.length >= 2 && gameState==false){
-            setTimeout(()=>gameStart(io), 500);
-        }
+        
+        setTimeout(()=>gameStart(io), 500);
+    
     });
 
     socket.on(commends.disconnect, () => {
@@ -91,10 +102,13 @@ export const socketController = (socket, io) => {
             if(user.socket == socket.id){
                 unlockColor(user.userColor)
                 userList.splice(index, 1);
+                const username = user.userId;
+                io.emit(commends.newMsg, {data : {...notice.freeNotice, text: `${username}님이 퇴장하셨습니다.`}});
             }
             scoreBoard(io);
             if(userList.length < 2 && gameState==true){
-                setTimeout(()=>gameEnd(io), 500);
+                io.emit(commends.newMsg, {data : {...notice.freeNotice, text: `게임이 종료되었습니다.`}});
+                gameEnd(io);
             }
         });
     });
@@ -112,8 +126,13 @@ export const socketController = (socket, io) => {
     });
     socket.on(commends.sendMsg, (data)=>{
         socket.broadcast.emit(commends.newMsg, {data})
-        console.log(data)
+        console.log(data.user)
         if(word === data.text){
+            userList.map((user)=>{
+                if(user.userId === data.user){
+                    user.score = user.score + 10;
+                }
+            })
             gameEnd(io, data.user);
         }
     })
